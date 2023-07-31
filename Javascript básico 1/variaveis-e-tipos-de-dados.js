b6d83adc0380cfdb38df614458281753bec51164
diego-salves <diegosantos57@outlook.com.br>
2023-07-31 20:05:29 -0300

    aula 01

---
+++ Javascript básico 1/variaveis-e-tipos-de-dados.js
@@ -0,0 +1,27 @@
+var nome1 = 'Rafael';
+var nome2 = "Damião";
+
+console.log(nome1)
+console.log(nome2)
+
+nome1 = 'Diego';
+nome2 = 108;
+
+console.log(nome1)
+console.log(nome2)
+
+//Tipos primitivos
+var nome = "Diego"; //string
+var idade = 26; //int
+var altura = 1.80; //float
+var legal = true; //boolean //true or false
+var compras = ["pão", "ovos", "leite"] //vetor (array)
+var compras_do_mes = {
+    padaria: ["pão", "leite", "ovos", "queijo"],
+    limpeza: ["detergente", "desinfetante"],
+    guloseimas: ["chocolate"],
+    saldo: 200
+}; //objeto
+
+console.log(compras[2])
+console.log(compras_do_mes.limpeza)
