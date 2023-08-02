11edad91c9235eafff0b607e3649a22a04495b29
diego-salves <diegosantos57@outlook.com.br>
2023-08-02 20:01:07 -0300

    Operadores aritméticos

---
+++ Javascript básico 1/operadores.js
@@ -0,0 +1,40 @@
+var num1 = 42, num2 = 19, nome = "Diego";
+
+console.log(num1, num2, nome);
+
+//soma
+var res = num1 + num2;
+console.log(res)
+
+//subtração
+
+res = num1 - num2;
+console.log(res)
+
+// Multiplicação
+res = num1 * num2;
+console.log(res);
+
+//Divisão
+res = num1 / num2;
+console.log(res)
+
+// Módulo (resto da divisão)
+res = num1 % 2;
+console.log(res)
+
+// if (num1 % 2 == 0){
+//     console.log("Par")
+// }else{
+//     console.log("ímpar")
+// }
+
+// Incremento-Decremento
+//num1 = num1 + 8;
+num1 += 8; // -=, *=, /=
+console.log(num1);
+
+console.log(num1++); //Imprime a variável e depois incrementa 50, 51
+console.log(num1--); //Imprime a variável e depois decrementa 51, 50
+console.log(++num1); //Incrementa e depois imprime 51
+console.log(--num1); //Decrementa e depois imprime 50
