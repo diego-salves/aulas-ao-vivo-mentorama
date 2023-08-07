af2a317ec513f902ba35405ac2390f3e2fddd19e
diego-salves <diegosantos57@outlook.com.br>
2023-08-07 19:25:56 -0300

    .

--- Javascript básico 1/operadores.js
+++ Javascript básico 1/operadores.js
@@ -38,3 +38,48 @@ console.log(num1++); //Imprime a variável e depois incrementa 50, 51
 console.log(num1--); //Imprime a variável e depois decrementa 51, 50
 console.log(++num1); //Incrementa e depois imprime 51
 console.log(--num1); //Decrementa e depois imprime 50
+
+//Operadores aritméticos
+/*
++ adição
+- subtração
+* multiplicação
+/ divisão
+
+abreviações
++=
+-=
+*=
+/=
+*/
+
+console.log(7/0) //Infinity
+
+console.log("42" / 2);
+
+console.log("Diego" * 2); //NaN (Not a number)
+
+var num = 42;
+
+console.log(num.toString());
+
+console.log(13.124412124.toFixed(2));
+
+console.log(Math.PI);
+
+console.log(Math.pow(3, 2));
+
+//console.log(3**2);
+
+console.log(typeof(42));
+
+console.log(typeof("Javascript"));
+
+console.log(Number.isInteger(45));
+console.log(Number.isInteger("java"));
+console.log(Number.isInteger(13.123123));
+
+// conversão binária
+
+var value = 105632143;
+console.log(value.toString(2));
