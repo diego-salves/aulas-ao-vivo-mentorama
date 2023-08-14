961f0f70437976b5098aa5a12eca6967da8f4603
diego-salves <diegosantos57@outlook.com.br>
2023-08-14 19:47:20 -0300

    intervalos e timeout

---
+++ introducao-javascript/intervalos-timeout.js
@@ -0,0 +1,13 @@
+//Intervalos e Timeout
+
+function escrever(){
+    console.log("Aula de Javascript")
+}
+
+// Intervalo
+//setInterval(escrever, 3000); //3 segundos
+
+//console.log("fim")
+
+//Timeout (tempo esgotado) //delay - atraso
+setTimeout(escrever, 5000); //3 segundos
